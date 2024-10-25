f13fe84160c237a52cc64c275d3bc59a24c05d89
Isidro Zertucha <isidro.zertucha@gmail.com>
2024-10-25 15:53:43 -0400

    fix colors

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 let theme = createTheme({
   palette: {
     primary: {
-      main: '#007878',
+      main: '#008989',
       light: '#00a8a8',
       dark: '#005c5c',
     },

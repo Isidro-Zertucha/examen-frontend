e00d0d77a9541b35c01c842e2d08dbd3f5f202a6
Isidro Zertucha <isidro.zertucha@gmail.com>
2024-10-25 00:36:05 -0400

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Task } from './interfaces/task'
-import {TaskItem, TaskList} from './components/'
+import {TaskList} from './components/'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [Tasks, setTasks] = useState<Task[]>([])
-
-  const exampleTask:Task = {
-    id: 3,
-    title: "Botar basura",
-    completed: false
-  }
   
-  //<TaskItem task={exampleTask}></TaskItem>
   return (
     <>
-      <TaskList></TaskList>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      
+      <TaskList></TaskList>      
     </>
   )
 }
